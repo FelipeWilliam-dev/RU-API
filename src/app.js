331e5e52d6dd09f6f90eda2b1bfbbc43e008c4ec
Felipe William <felipe.william.silva@outlook.com>
2025-10-27 20:19:03 -0400

    :sparkles: feat: add rotas protegidas para admin

--- src/app.js
+++ src/app.js
@@ -1,11 +1,20 @@
+require('dotenv').config();
 const express = require('express');
 const path = require('path');
 const hbs = require('hbs');
 const bodyParser = require('body-parser');
+const session = require('express-session');
 
 const routes = require('./routes');
 
 const app = express();
+
+app.use(session({
+    secret: process.env.SESSION_SECRET, 
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false, maxAge: 3600000 } 
+}));
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,8 +3,10 @@ const router = express.Router();
 
 const cardapioRoutes = require('./cardapioRoutes');
 const apiRoutes = require('./apiRoutes');
+const adminRoutes = require('./adminRoutes');
 
 router.use('/', cardapioRoutes);  
-router.use('/api', apiRoutes);     
+router.use('/api', apiRoutes);
+router.use('/admin', adminRoutes);     
 
 module.exports = router;
